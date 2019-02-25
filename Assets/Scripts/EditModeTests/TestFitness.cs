f590609d3aae8cddb68cfa81b4d1ab30249c36cc
eadventurous <e.o.ivkov@gmail.com>
2019-02-25 13:18:38 +0300

    Fitness GS class

---
+++ Assets/Scripts/EditModeTests/TestFitness.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using GeneticSharp.Domain.Chromosomes;
+
+namespace Tests
+{
+    public class TestFitness
+    {
+        private const double _errorMargin = 0.001;
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestEquality()
+        {
+            var fitness = new LevelChunkFitness(0);
+
+            // Use the Assert class to test conditions
+            var chromosome = new LevelChunkChromosome(10);
+            Assert.LessOrEqual(Mathf.Abs((float)fitness.Evaluate(chromosome)
+                - (float)fitness.Evaluate(chromosome.Clone())), _errorMargin);
+        }
+
+        /// <summary>
+        /// Test Increase Lower Target
+        /// </summary>
+        [Test]
+        public void TestIncreaseLT()
+        {
+            var fitness = new LevelChunkFitness(0);
+
+            // Use the Assert class to test conditions
+            var chromosome1 = new LevelChunkChromosome(2);
+            chromosome1.ReplaceGenes(0, new Gene[] { new Gene(LevelElement.None), new Gene(LevelElement.None) });
+
+            var chromosome2 = new LevelChunkChromosome(2);
+            chromosome2.ReplaceGenes(0, new Gene[] { new Gene(LevelElement.None), new Gene(LevelElement.Beast) });
+
+            Assert.Less(fitness.Evaluate(chromosome2), fitness.Evaluate(chromosome1));
+        }
+
+        /// <summary>
+        /// Test Increase Higher Target
+        /// </summary>
+        [Test]
+        public void TestIncreaseHT()
+        {
+            var fitness = new LevelChunkFitness(10);
+
+            // Use the Assert class to test conditions
+            var chromosome1 = new LevelChunkChromosome(2);
+            chromosome1.ReplaceGenes(0, new Gene[] { new Gene(LevelElement.None), new Gene(LevelElement.None) });
+
+            var chromosome2 = new LevelChunkChromosome(2);
+            chromosome2.ReplaceGenes(0, new Gene[] { new Gene(LevelElement.None), new Gene(LevelElement.Beast) });
+
+            Assert.Less(fitness.Evaluate(chromosome1), fitness.Evaluate(chromosome2));
+        }
+    }
+}
---
+++ Assets/Scripts/LevelChunkFitness.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using GeneticSharp.Domain.Fitnesses;
+using GeneticSharp.Domain.Chromosomes;
+
+public class LevelChunkFitness : IFitness
+{
+    public float TargetDifficulty
+    {
+        get;
+        private set;
+    }
+
+    public LevelChunkFitness(int targetDifficulty)
+    {
+        TargetDifficulty = targetDifficulty;
+    }
+
+    public double Evaluate(IChromosome chromosome)
+    {
+        var difficulty = 0;
+        var genes = chromosome.GetGenes();
+        for (var i = 0; i < genes.Length - 1; i++)
+        {
+            var type1 = ((LevelElement)genes[i].Value).GetElementType();
+            var type2 = ((LevelElement)genes[i+1].Value).GetElementType();
+            if (type1 == type2 && type1 > 0)
+                difficulty += 2;
+            else if (Mathf.Abs(type1 - type2) == 1 && type1 > 0 && type2 > 0)
+                difficulty += 3;
+            else if (type2 - type1 >= 1)
+                difficulty += 1;
+        }
+        return -(Mathf.Abs(TargetDifficulty - difficulty));
+    }
+}
