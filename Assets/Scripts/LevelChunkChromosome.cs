bb3ee132a3bf1ab978bf25a675fcc08b68e785c3
eadventurous <e.o.ivkov@gmail.com>
2019-02-25 12:12:35 +0300

    LevelChunkChromosome definition for GeneticSharp

---
+++ Assets/Scripts/LevelChunkChromosome.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Randomizations;
+
+
+public class LevelChunkChromosome : ChromosomeBase
+{
+    private readonly int enemyTypesCount = System.Enum.GetNames(typeof(LevelElement)).Length;
+
+    public LevelChunkChromosome(int length) : base(length)
+    {
+        var enemies = RandomizationProvider.Current.GetInts(length, 0, enemyTypesCount);
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            ReplaceGene(i, new Gene((LevelElement)enemies[i]));
+        }
+    }
+
+    public override IChromosome CreateNew() => new LevelChunkChromosome(Length);
+
+    public override Gene GenerateGene(int geneIndex) => new Gene((LevelElement)RandomizationProvider.Current.GetInt(0, enemyTypesCount));
+}
+
